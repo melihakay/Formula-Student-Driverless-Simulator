eca11dabff6397524971cd424f6eb2a400404d24
Melih Akay <melihhakay@gmail.com>
2024-10-27 23:04:46 +0300

    switched to tmuxinator

--- install_wheelie_sim.sh
+++ install_wheelie_sim.sh
@@ -15,50 +15,12 @@ wget https://github.com/FS-Driverless/Formula-Student-Driverless-Simulator/relea
 unzip fsds-v2.2.0-linux.zip -d engine
 rm fsds-v2.2.0-linux.zip
 
-# Zellij
-case $(uname -m) in
-    "x86_64"|"aarch64")
-        arch=$(uname -m)
-    ;;
-    "arm64")
-        arch="aarch64"
-    ;;
-    *)
-        echo "Unsupported cpu arch: $(uname -m)"
-        exit 2
-    ;;
-esac
+# Tmuxinator
 
-case $(uname -s) in
-    "Linux")
-        sys="unknown-linux-musl"
-    ;;
-    "Darwin")
-        sys="apple-darwin"
-    ;;
-    *)
-        echo "Unsupported system: $(uname -s)"
-        exit 2
-    ;;
-esac
-
-url="https://github.com/zellij-org/zellij/releases/latest/download/zellij-$arch-$sys.tar.gz"
-curl --location "$url" | sudo tar -C "/bin/" -xz
-if [[ $? -ne 0 ]]
-then
-    echo
-    echo "Extracting binary failed, cannot launch zellij :("
-    echo "One probable cause is that a new release just happened and the binary is currently building."
-    echo "Maybe try again later? :)"
-    exit 1
-fi
-
-# Wheelie Sim
-
-
+sudo apt install tmuxinator btop
 
 # Symlinks
-sudo ln -s ~/Formula-Student-Driverless/wheelie_sim/run.sh /usr/local/bin/wheelieSim
+sudo ln -s ~/Formula-Student-Driverless/wheelie_sim/start.sh /usr/local/bin/wheelieSim
 
 echo 'Installation complete'
 echo ''
